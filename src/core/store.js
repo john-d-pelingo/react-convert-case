05be113bd06000e17f3a6986adf51598a17b03de
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2017-06-07 01:30:39 +0300

    Add redux-logger in dev environment (Resolves #9)

--- src/core/store.js
+++ src/core/store.js
@@ -1,20 +1,23 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducers from './reducers';
 
 export default (initialState = {}) => {
-  let middleware = applyMiddleware(thunk);
+  let middlewares = applyMiddleware(thunk);
 
   if (process.env.NODE_ENV === 'development') {
-    // Configure redux-devtools-extension
+    middlewares = applyMiddleware(thunk, logger);
+
+    // Configure redux-devtools-extension.
     // @see https://github.com/zalmoxisus/redux-devtools-extension
     const devToolsExtension = window.devToolsExtension;
     if (typeof devToolsExtension === 'function') {
-      // Compose all of our middleware one after another
-      middleware = compose(middleware, devToolsExtension());
+      // Compose all of our middlewares one after another.
+      middlewares = compose(middlewares, devToolsExtension());
     }
   }
 
-  return createStore(reducers, initialState, middleware);
+  return createStore(reducers, initialState, middlewares);
 };
